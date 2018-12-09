2d9b265f652d3fe80fa1c7ee52b0cb8a9d947000
ZBcoder <ZBcoder@aliyun.com>
2018-12-09 13:33:16 +0800

    .

---
+++ Searching/binarySearching.js
@@ -0,0 +1,13 @@
+/**
+ * 给定一个顺序表，查找关键词value索引
+ * 输入: arr = [1,4,7,11], value = 4
+ * 输出: return 1
+ */
+const binarySearching = (arr, value,low = 0,high)=> {
+    if(high === undefined) high = arr.length - 1
+    if(low === high && arr[low] !== value && arr[high] !== value) return null
+    const mid = parseInt((low + high) / 2)
+    if(arr[mid] === value) return mid
+    if(arr[mid] > value) return binarySearching(arr, value, low, mid)
+    return binarySearching(arr, value, mid, high)
+}
