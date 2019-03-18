52d4678d2a8622a923f6f6241b0867596974fa3c
demystify <zhangbo@zhihu.com>
2019-03-18 14:51:58 +0800

    目录重构

--- 1.Array.E-twoSum.js
+++ 1.Array.E-twoSum.js
@@ -1,9 +1,8 @@
 /**
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- * 输入： nums = [2, 7, 11, 15], target = 9,
- * 解析： nums[0] + nums[1] = 2 + 7 = 9,
- * 输出： return [0, 1].
+ * input： nums = [2, 7, 11, 15], target = 9,
+ * output： return [0, 1].
  */
 
 /**
