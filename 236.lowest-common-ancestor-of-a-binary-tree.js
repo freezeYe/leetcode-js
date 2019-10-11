76bfa2ca84dd4611c03551a086c6585e3614deb6
zhangbo <zbcoder@aliyun.com>
2019-10-11 23:52:13 +0800

    19/10/11

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=236 lang=javascript
+ *
+ * [236] Lowest Common Ancestor of a Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+  if (!root || p === root || q === root) return root
+  const left = lowestCommonAncestor(root.left, p, q)
+  const right = lowestCommonAncestor(root.right, p, q)
+  if (left && right) return root
+  return left ? left : right
+};
+// @lc code=end
+
